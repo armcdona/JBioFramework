60bd57b6d923ccb62f100fde65e6a9764850c1d5
Aidan Sawyer <aidan.keefe.s@gmail.com>
2013-01-29 19:32:20 -0500

    Simple commit which includes the Welcome Tab which extends JPanel, is visible in the tabbed pane, has -very- limited operations (bare bones), and is essentially just a proof of concept until we're done with refactoring.
    The delay is due to git issues.

--- svnrepo/RIT_JBF_source/src/Tutorial.java
+++ svnrepo/RIT_JBF_source/src/Tutorial.java
@@ -4,18 +4,15 @@
  *  # calls: -none-
  *  # called by: /Welcome/
  *  # envelop: /Welcome/
- *  # package: /Welcome/
- *  ! needs to be written/populated. 
+ *  ! needs to be written/populated.
  *  ! need to take commands from /Welcome/ lP
  */
 
 import javax.swing.*;
 
 public class Tutorial extends JPanel{
-	/*instance variables*/
 	private JLabel message;
-	
-	
+
 	public Tutorial(){
 		message = new JLabel("tutorial class");
 		this.add(message);
--- svnrepo/RIT_JBF_source/src/Welcome.java
+++ svnrepo/RIT_JBF_source/src/Welcome.java
@@ -35,8 +35,7 @@ public class Welcome extends JPanel{
 	private JPanel headP; //head panel with about and problem sheets?
 	private JLabel head; // simple welcome text
 	private JButton help; // opens up site -- will be outdated eventually %%
-	private JButton about; //button 
-//	private JButton aboutProc;
+	private JButton about; //button
 
 	/*Tutorial interface panel*/
 	private JPanel tutPan; //can you stack panels like that? // next+previous, search, etc.
@@ -59,27 +58,23 @@ public class Welcome extends JPanel{
 	private JPanel tailP; //tail panel with contact and credits
 	private JButton credits; //
 	private JButton contact; // displays email %%not copy-able
-	private JButton probSheets; // will eventually 
-//	private JFrame welcomeFrame;
+	private JButton probSheets; // will eventually
 	
 	//////////////////////////////
 	public Welcome(){
 	//////////////////////////////
-//		super();
 		GridBagConstraints c = new GridBagConstraints();
-//		GridBagConstraints cons = new GridBagConstraints();
 
 		
 		/*left panel*/
 		lP = new JPanel(new GridBagLayout());
-		lP.setBackground(Color.gray);
-		//lP.getSize();
+		lP.setBackground(Color.gray);;
 
+//	    c.insets = new Insets(<top>, <bottom>, <left>, <right>)
 	    c.insets = new Insets(5, 5, 5, 5); //spacing between
 //	    c.fill = GridBagConstraints.HORIZONTAL;
 		c.weightx = 1.0;
-//	    c.weighty = 1.0;
-//	    c.insets = new Insets(<top>, <bottom>, <left>, <right>)
+
 	    
 	    this.add(lP,c);
 	    
@@ -88,10 +83,8 @@ public class Welcome extends JPanel{
 	    headP.setBackground(Color.gray);
 
 		head = new JLabel("Welcome to JBioFramework!");  //Header
-//		c.gridx = 1;
 		c.anchor = GridBagConstraints.CENTER;
 		c.gridy = 0;
-//		headP.add(head,c);
 		lP/*headP*/.add(head,c);
 	    
 		help = new JButton("Help");
@@ -107,7 +100,6 @@ public class Welcome extends JPanel{
 			        }  
 			}
 		});
-//		headP.add(help,c);
 		lP/*headP*/.add(help,c);
 
 		about = new JButton("About");
@@ -122,59 +114,37 @@ public class Welcome extends JPanel{
 			            System.err.println(i.getMessage());
 			            i.printStackTrace();
 			    }
-			    
-				JOptionPane.showMessageDialog(null, "JBioFramework (JBF) is a set of simulations of " +
-						" different chemical separations applications frequently used in chemistry, biochemistry, and proteomics research.");
-
-				
-//				JOptionPane.showMessageDialog(null, "JBioFramework (JBF) is a set of simulations of " +
-//						"three different chemical separations applications " +
-//						"(2D electrophoresis, Mass Spectroscopy, ) " /n +
-//						"that are frequently used in chemistry, biochemistry and proteomics research.");
-			}
-		});
+                }
+		    });
 		lP/*headP*/.add(about,c);
-		
-//	    lP.add(headP,c);
 
 		
 		/*tutPan*/
 		prev = new JButton("Previous");
-//		c.gridx = 1;
 		c.gridy = 3;
 		c.anchor = GridBagConstraints.CENTER;
-//		tutPan.add(prev,c);
 		lP/*tutPan*/.add(prev,c);
 		
 		next = new JButton("Next");
-//		c.gridx = 1;
 		c.gridy = 3;	
 		c.anchor = GridBagConstraints.LINE_START;
 	    c.insets = new Insets(5, 5, 5, 5); //spacing between
-//	    tutPan.add(prev,c);
 		lP/*tutPan*/.add(next,c);
 		
 		search = new JTextField("[Search Help Files]"); //exists currently as a text field
 		c.gridy = 2;
 //		c.gridwidth = 2;
 		c.fill = GridBagConstraints.HORIZONTAL;
-//		tutPan.add(search,c);
 		lP/*tutPan*/.add(search,c);
 		
 		searchButton = new JButton("Search");
-//		c.gridx = 2;
 		c.fill = GridBagConstraints.NONE;
 		c.anchor = GridBagConstraints.LINE_END;
 		c.gridy = 2;
-//		tutPan.add(searchButton,c);
 		lP/*tutPan*/.add(searchButton,c);
-		
-//		lP.add(tutPan,c);
-		
 		
 		/*tailP*/
 		probSheets = new JButton("Problem Sheets");
-//		c.gridx = 1;
 		c.anchor = GridBagConstraints.CENTER;
 		c.gridy = 4;
 		probSheets.addActionListener(new ActionListener(){
@@ -186,7 +156,6 @@ public class Welcome extends JPanel{
 				 */
 			}
 		});
-//		tailP.add(probSheets,c);
 		lP/*tailP*/.add(probSheets,c);
 	
 		contact = new JButton("Contact Us");
@@ -195,10 +164,8 @@ public class Welcome extends JPanel{
 				JOptionPane.showMessageDialog(null,"paul.craig.rit.edu");
 			}
 		});
-//		c.gridx = 1;
 		c.anchor = GridBagConstraints.CENTER;
 		c.gridy = 5;
-//		tailP.add(contact,c);
 		lP/*tailP*/.add(contact,c);
 		
 		credits = new JButton("Credits");
@@ -211,24 +178,14 @@ public class Welcome extends JPanel{
 				JOptionPane.showMessageDialog(null, "matlab and intelliJ");
 			}
 		});
-//		tailP.add(credits,c);
 		lP/*tailP*/.add(credits,c);
-		
-//		lP.add(tailP,c);
- 
+
 		/*right panel*/
 		rP = new JPanel();
 		rP.setBackground(Color.darkGray);
-//		rP.setLayout(new BoxLayout(rP, BoxLayout.Y_AXIS));
 		rP.setLayout(new GridBagLayout());
 		rP.setSize(900,700);
-	    
-	    c.gridy = 0;
-//	    this.add(rP, c);
-		
-//		tutorial = new JLabel("JBioFramework is an application developed at RIT " +
-//				"designed to help educate its users about various" +
-//				"scientific procedures. To find out more, click the button below");
+
 		tutorial = new Tutorial(); //JLabel("Main tutorial window");
 		c.gridy = 0;
 //		c.anchor = GridBagConstraints.CENTER;
@@ -241,10 +198,5 @@ public class Welcome extends JPanel{
 		this.add(lP);
 		this.add(rP);
         this.setVisible(true);
-//        this.setSize(550,500);
-//        this.setResizable(true);
-//        super.setDefaultCloseOperation(EXIT_ON_CLOSE);
-//		this.add(lP); //??
-//		this.add(rP); //??
 	}   
 }
