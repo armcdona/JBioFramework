8f5bf7b97b746010f19502afb7451955d6d74ab2
emilysekera <emily.sekera@gmail.com>
2013-02-14 19:15:05 -0500

    In this update, the initial movement to adding chemical drawings from mass spectrometry fragments is added. In this section we have the amino acid translator which looks at the fragment given by ion and decides what name to give the acid. In ion the added feature of a text box that pops up with the name of the amino acid of the fragment that is displayed is shown. Lastly, the marvin tab is started in which a pane opens that displays a marvin sketch window. Also added is some changes to the html writing in the about page.

---
+++ svnrepo/RIT_JBF_source/src/MarvinTab.java
@@ -0,0 +1,69 @@
+import java.awt.Dimension;
+
+import javax.swing.BoxLayout;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.SwingUtilities;
+import javax.swing.WindowConstants;
+
+import chemaxon.marvin.beans.MSketchPane;
+import chemaxon.marvin.common.UserSettings;
+import chemaxon.marvin.sketch.SketchParameterConstants;
+
+public class MarvinTab {
+
+	private JTextArea textarea = new JTextArea(10,50);
+
+	private MSketchPane createSketchPane() {
+		MSketchPane pane = new MSketchPane(createUserSettings());
+		pane.setMol("AspGluLysArg");
+		pane.setPreferredSize(new Dimension(500, 500));
+		return pane;
+	}
+
+	private UserSettings createUserSettings() {
+		UserSettings settings = new UserSettings(this.getClass().getResourceAsStream("marvin.properties"));
+		settings.setProperty(SketchParameterConstants.MENU_CUSTOMIZATION_FILE,
+				System.getProperty("user.dir") + "/src/customization.xml");
+		settings.setProperty(SketchParameterConstants.SHORTCUTS,
+				System.getProperty("user.dir") + "/src/shortchuts.xml");
+		settings.setProperty(SketchParameterConstants.TOOLBAR_TEMPLATES + "20",
+				":specials:specialTemplates.mrv");
+		return settings;
+	}
+
+	private JComponent createMainPanel() {
+		JPanel topPanel = new JPanel();
+		MSketchPane sketch = createSketchPane();
+		JPanel sketchPanel = new JPanel();
+		sketchPanel.add(sketch);
+		
+		topPanel.add(sketchPanel);
+
+		JPanel mainPanel = new JPanel();
+		mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+		mainPanel.add(topPanel);
+		return mainPanel;
+	}
+
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				createAndShowGUI();
+			}
+		});
+	}
+
+	private static void createAndShowGUI() {
+		JFrame frame = new JFrame();
+		MarvinTab example = new MarvinTab();
+		frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+		frame.add(example.createMainPanel());
+		frame.pack();
+		frame.setVisible(true);
+	}
+}
