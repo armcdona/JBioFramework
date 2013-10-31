cfb1fd112a6441635f34faa015e608d8214d6fd8
Aidan Sawyer <aks5238@rit.edu>
2013-10-31 15:48:35 -0400

    prelim refactor /MessageFrame/. *with notes*
    -converted AWT to Swing
    -added main class for unit-testing.
    -added docs and comments
    
    -notes-
    1) this class is not entirely functional.
      -it doesn't actually have a constructor which takes in any
       string to display. so will always be empty
    2) it's not entirely helpful/necessary
      -all functionality it does have is already contained in java's
       JOptionPane methods (showMessageDialog, showInputDialog, etc.).
    
    This class should be deleted. I haven't yet because I need to look
    more into where/if it's used and update its calls.

--- svnrepo/RIT_JBF_source/src/MessageFrame.java
+++ svnrepo/RIT_JBF_source/src/MessageFrame.java
@@ -23,28 +23,36 @@
  * @author Jill Zapoticznyj
  */
 
-import java.awt.Frame;
-import java.awt.Panel;
-import java.awt.Label;
+//GUI components
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+
+//event listeners
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-public class MessageFrame extends Frame {
+
+public class MessageFrame extends JFrame {
 
  /** variables for the file reading pop-up frame **/
 
-    private Panel thePanel;              //panel to add components to
-    private Label theLabel;              //label to display message on
+    private JPanel panel;              //panel to add components to
+    private JLabel label;              //label to display message on
     private String theMessage = "";      //message to display to user
-    
-    //construct simple GUI
+
+    /**
+     * construct and display a simple GUI
+     */
     public MessageFrame() {
-	
-	//set the title to Alert
-	setTitle("Alert");
-	thePanel = new Panel();          //init components
+
+        //set the title to Alert
+	  super("Alert");
+
+	//initialize components
+	  panel = new JPanel();          //init components
 	//position the message label to be centered in the frame
-	theLabel = new Label(theMessage, Label.CENTER);
-	
+	  label = new JLabel(theMessage, JLabel.CENTER);
+
 	// allow the user to close the frame
 	this.addWindowListener(new WindowAdapter() {
 		public void windowClosing(WindowEvent e) {
@@ -54,17 +62,27 @@ public class MessageFrame extends Frame {
 
 	// set the sizes of the frame and its components
 	this.setBounds(20,140,600,100);
-	thePanel.setBounds(0,0,600,100);
-	theLabel.setBounds(0,0,600,100);
-	
+	panel.setBounds(0,0,600,100);
+	label.setBounds(0,0,600,100);
+
 	//add the components
-	this.add(thePanel);
-	thePanel.add(theLabel);
+	this.add(panel);
+	panel.add(label);
+
+        super.setVisible(true);
     }
+
     //set the message
     public void setMessage(String message) {
 	theMessage = message;
-	theLabel.setText(theMessage);
+	label.setText(theMessage);
+    }
+
+    /**
+     * simple unittest for functionality
+     */
+    public static void main(String[] args){
+	MessageFrame mFrameTest = new MessageFrame();
     }
 }//MessageFrame
 
