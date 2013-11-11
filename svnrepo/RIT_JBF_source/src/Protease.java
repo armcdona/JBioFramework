9a18ecd491b40c6339e1db49b252c55f93ac2630
Aidan Sawyer <aks5238@rit.edu>
2013-11-11 13:58:52 -0500

    verify /Protease/ and /ProteaseException/ separate
    spoke with CS professor about whether /Protease/ and /ProteaseException/ should
    be separate files or if they should be integrated into one. they
    -should- be separate classes, just grouped into the |Proteins| package
    later on.

--- svnrepo/RIT_JBF_source/src/Protease.java
+++ svnrepo/RIT_JBF_source/src/Protease.java
@@ -14,8 +14,10 @@
  */
 
 /**
- * Protease is the abstract class inherited by all classes that are responsible for
- * cutting a protein's sequence at specific points. Only method they are required
+ * Protease is the abstract class inherited by all classes that are responsible
+ * for cutting a protein's sequence at specific points.
+ *
+ * Only method they are required
  * to have is cut.
  *
  * |proteins|
