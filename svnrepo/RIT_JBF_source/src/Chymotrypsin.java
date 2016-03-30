874388a2aa82d8350ef72d3f1c4f74e49f2ac8fd
joshuapouliot27 <joshuapouliot27@gmail.com>
2016-03-30 12:32:45 -0400

    Updated Protease and expanded classes

--- svnrepo/RIT_JBF_source/src/Chymotrypsin.java
+++ svnrepo/RIT_JBF_source/src/Chymotrypsin.java
@@ -1,49 +1,18 @@
-/*
- * Chymotrypsin cuts a peptide sequence at Tyrosin(Y), Tryptophan(W), or
- * Phenylalanine(F).
- *
- */
-
 /**
  *
  * @author Amanda Fisher
  */
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
- * Cuts proteins in a similar/same way that Chymotrypsin does
+ * Cuts proteins in a similar/same way that Chymotrypsin does (after Tyrosin(Y), Tryptophan(W), and Phenylalanine(F))
  */
 public class Chymotrypsin extends Protease {
 
     public Chymotrypsin() {
-        String[] cutAminoAcids = {"Y", "W", "F"};
+        cutAminoAcids = new ArrayList<>(Arrays.asList('Y','W','F')); //Chymotrypsin cuts at these proteins
+        cutsBefore = false;
     }
-
-    /**
-     * The cut method takes an input sequence and cuts it in to different Strings at points dependent on the type of Protease using the method.
-     * It uses the makeIon method to turn the ArrayList of collected characters in to a String
-     * @param sequence String sequence representing an amino acid chain
-     * @return The sequence after it is cut
-     * @throws ProteaseException Thrown when given an input with incorrect formatting
-
-    public ArrayList<String> cut(String sequence) throws ProteaseException {
-        if (sequence.contains(" ")) {
-            throw new ProteaseException("Sequence to be cut must not contain spaces.");
-        } else if (sequence.matches(".*\\d.*")) {
-            throw new ProteaseException("Sequence to be cut must not contain numbers.");
-        } else if (sequence.matches(".*[a-z].*")) {
-            throw new ProteaseException("Sequence to be cut must contain all upper case letters.");
-        }
-
-        char[] charSequence = sequence.toCharArray();
-        for(int i = 0; i < charSequence.length; i++) {
-            buildingIons.add(charSequence[i]);
-            if(charSequence[i] == 'Y' || charSequence[i] == 'W' || charSequence[i] == 'F') {
-                makeIon();
-            }
-        }
-        makeIon();
-        return cutSequence;
-    }*/
 
 }
