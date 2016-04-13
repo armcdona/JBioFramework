2ad034b68901bee871c7e1710fbe0518eb670edb
Joshua Pouliot <joshuapouliot27@gmail.com>
2016-04-13 00:01:05 -0400

    updated Protease

--- svnrepo/RIT_JBF_source/src/Protease.java
+++ svnrepo/RIT_JBF_source/src/Protease.java
@@ -72,14 +72,6 @@ public class Protease {
      * Takes the characters collected by cut and turns them in to a string that represents the sequence of the Ion
      */
     public void makeIon(ArrayList<Character> buildingIons,ArrayList<String> cutSequence) {
-        /**
-         Character[] characterIon = new Character[buildingIons.size()];//makes a character array from building Ions
-         characterIon = buildingIons.toArray(characterIon);//sets it to buildingions
-         Character[] charIon = new Character[characterIon.length];//new charion for the ion
-         for(int j = 0; j < characterIon.length; j++) {
-         charIon[j] = characterIon[j].charValue();
-         }
-         */
         String ion = new String();
         for (Character c : buildingIons) {
             ion += c.toString();
@@ -87,17 +79,6 @@ public class Protease {
         cutSequence.add(ion);
         buildingIons.clear();
     }
-    /** example of the cut method
-     char[] charSequence = sequence.toCharArray();
-     for(int i = 0; i < charSequence.length; i++) {
-     buildingIons.add(charSequence[i]);
-     if(charSequence[i] == 'Y' || charSequence[i] == 'W' || charSequence[i] == 'F') {
-     makeIon();
-     }
-     }
-     makeIon();
-     return cutSequence;
-     */
 
 
 }
