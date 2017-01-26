3d65070644159aca55e3db298228b936b777b67d
joshuapouliot27 <joshuapouliot27@gmail.com>
2017-01-26 13:11:14 -0500

    Configured all simulations to correctly boot up and be added into the MainWindow class.

---
+++ jbioframework-massspec/src/main/java/org/jbioframework/massspec/MainWindow.java
@@ -0,0 +1,45 @@
+package org.jbioframework.massspec;
+
+
+import org.apache.log4j.BasicConfigurator;
+import org.jbioframework.library.gui.MarvinTab;
+import org.jbioframework.library.gui.WelcomeWindow;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainWindow extends JFrame {
+
+    public MainWindow() {
+        super("2D Electrophoresis");
+        this.setName("JBioFramework - 2D Electrophoresis");
+        BasicConfigurator.configure();
+        JTabbedPane window = new JTabbedPane();
+        window.add("Welcome", new WelcomeWindow());
+        window.add("Mass Spectroscopy", new MassSpecMain());
+        window.add("Marvin Sketch", new MarvinTab().createMainPanel());
+
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+        }
+
+        add(window);
+        super.pack();
+        super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        super.setPreferredSize(Toolkit.getDefaultToolkit().getScreenSize());
+        super.setVisible(true);
+
+    }
+
+    public static void main(String[] args) {
+        new MainWindow();
+    }
+}
