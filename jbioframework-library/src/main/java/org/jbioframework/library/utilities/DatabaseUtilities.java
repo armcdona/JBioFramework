50f76badaaa0b55187b1521b830b64a92e35259f
joshuapouliot27 <joshuapouliot27@gmail.com>
2017-02-07 12:05:50 -0500

    Added database class with utility functions, and logging class with code needed to log messages into log file.

--- jbioframework-library/src/main/java/org/jbioframework/library/utilities/DatabaseUtilities.java
+++ jbioframework-library/src/main/java/org/jbioframework/library/utilities/DatabaseUtilities.java
@@ -2,13 +2,52 @@ package org.jbioframework.library.utilities;
 
 import org.jbioframework.library.protein.Protein;
 
+import java.io.File;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Vector;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.sqlite.*;
+
 
 public class DatabaseUtilities {
 
-    public static Vector<Protein> loadDatabase() {
+    final static Logger logger = LoggerFactory.getLogger(DatabaseUtilities.class);
+
+    public static Vector<Protein> loadDatabase(File databaseFile) {
         Vector<Protein> proteins = new Vector<Protein>();
+        Connection databaseConnection = connect(databaseFile.getAbsolutePath());
         return proteins;
+    }
+
+    public static void saveDatabase(Vector<Protein> proteins) {
+
+    }
+
+    private static Connection connect(String absoluteFilePath) {
+        String db_URL = "jdbc:sqlite:" + absoluteFilePath;
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(db_URL);
+            if (connection == null) {
+                logger.error("Could not connect to sqlite database "+absoluteFilePath);
+            } else {
+                logger.info("Successfully connected to sqlite database "+absoluteFilePath);
+            }
+        } catch (SQLException e) {
+            logger.error(e.getMessage());
+        }
+        return connection;
+    }
+
+    private static String constructSQLTableCreationCommand(String tableName, Vector<String> columnName,
+                                                           Vector<String> columnParameters, Vector<String> columnType) {
+        String sqlCommand = "";
+        return sqlCommand
     }
 
 }
---
+++ jbioframework-library/src/main/java/org/jbioframework/library/utilities/Logging.java
@@ -0,0 +1,6 @@
+package org.jbioframework.library.utilities;
+
+public class Logging {
+    //final Logger logger = LoggerFactory.getLogger(Example.class);
+    //This piece of code must be placed into each class that needs logging capabilities
+}
