09911380642a063ed0a6574f50217a07a9e9103c
Joshua Pouliot <joshuapouliot27@gmail.com>
2017-02-07 08:52:32 -0500

    Added Database utilities class.

---
+++ jbioframework-library/src/main/java/org/jbioframework/library/utilities/DatabaseUtilities.java
@@ -0,0 +1,14 @@
+package org.jbioframework.library.utilities;
+
+import org.jbioframework.library.protein.Protein;
+
+import java.util.Vector;
+
+public class DatabaseUtilities {
+
+    public static Vector<Protein> loadDatabase() {
+        Vector<Protein> proteins = new Vector<Protein>();
+        return proteins;
+    }
+
+}
